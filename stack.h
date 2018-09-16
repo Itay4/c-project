63a8f572aad542db149fc8cd19ab113aca4bb9ee
Itay4 <31018228+Itay4@users.noreply.github.com>
2018-09-16 20:30:52 +0300

    Update stack.h

--- stack.h
+++ stack.h
@@ -13,14 +13,25 @@
 
 #include "main_aux.h"
 
+/**
+ * @brief element structure representing an element in a stack.
+ * element contains data field which contains an array of integers.
+ * element contains board field which contains a sudoku board.
+ * element contains next field which points to the next element in the stack.
+ */
 typedef struct element {
     int* data;
     cell** board;
     struct element *next;
 }element;
 
-typedef struct stack_t
-{
+
+/**
+ * @brief element structure representing a stack.
+ * stack contains counter field which indicates the number of elements in a stack.
+ * stack contains top field which points to the top element in the stack.
+ */
+typedef struct stack_t {
     int counter;
     element *top;
 }stack;
