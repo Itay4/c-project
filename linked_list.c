87485dc9695e845f5d7504c397305b24ec8216fc
rotemb1 <36661464+rotemb1@users.noreply.github.com>
2018-09-16 19:29:32 +0300

    Add files via upload

--- linked_list.c
+++ linked_list.c
@@ -1,13 +1,27 @@
+/** @file linked_list.c
+ *  @brief linked list source file.
+ *
+ *  This module encapsulates the sudoku puzzle game moves list represented by a linked list.
+ *  Includes functions to match all game commands related to moves list to be entered by the user (undo, redo, reset).
+ *  Includes auxiliary functions that are relevant specifically to linked_list structure.
+ *
+ *  @author Itay Keren (itaykeren)
+ *  @author Rotem Bar (rotembar)
+ *
+ */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+/* -- Includes -- */
 #include "linked_list.h"
 #include "main_aux.h"
 #include "game.h"
-#include "parser.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 
+/* Allocates memory and creates returns a node to be later on added to game moves list represented by a linked list.
+ * node board field is updated with given sudoku board (board after a changing command was executed).
+ * prev and next field are updated to null and will be updated in the future when added to a list */
 node* create_node(cell** board) {
     node* newNode = malloc(sizeof(node));
     if (newNode == NULL) {
@@ -19,7 +33,10 @@ node* create_node(cell** board) {
     newNode->next = NULL;
     return newNode;
 }
-
+/* Allocates memory and creates and returns a linked list which intend to represent game moves list
+ * first node in the list (head of list) is created based on given sudoku board (first board in current game)
+ * fields head and current are updated to point on head of list,
+ * tail is updated to null to mark a newly created list which only contain head*/
 list* create_list(cell** board) {
     list* lst = malloc(sizeof(list));
     if (lst == NULL) {
@@ -32,49 +49,7 @@ list* create_list(cell** board) {
     return lst;
 }
 
-void delete_next_nodes(node* newTail, list* lst) {
-    node* currentToDel;
-    if (newTail == NULL){
-        return;
-    }
-    currentToDel = lst->current->next;
-    newTail->next = NULL;
-    newTail->prev = lst->current;
-    lst->tail = newTail;
-    lst->current->next = newTail;
-    lst->current = newTail;
-    if (currentToDel == NULL) {
-        return; /*no nodes to delete*/
-    } else {
-        while (currentToDel->next != NULL) {
-            currentToDel = currentToDel->next;
-            free_board(currentToDel->prev->board);
-            free(currentToDel->prev);
-        }
-    }
-    free_board(currentToDel->board);
-    free(currentToDel);
-}
-
-void insert_at_tail(cell** board, list* lst) {
-    node* temp = lst->current;
-    node* newNode = create_node(board);
-    if (temp == lst->head) { /*first move*/
-        lst->head->next = newNode;
-        newNode->prev = lst->head;
-        lst->current = newNode;
-        lst->tail = newNode;
-        return;
-    } else if (temp != lst->tail){
-        delete_next_nodes(newNode, lst);
-    } else {
-        lst->tail->next = newNode;
-        newNode->prev = lst->tail;
-        lst->tail = newNode;
-        lst->current = newNode;
-    }
-}
-
+/* Frees given moves list by freeing memory of all nodes and list itself */
 void free_list(list* lst) {
     node* currentToDel = lst->head;
     while (currentToDel->next != NULL) {
@@ -88,6 +63,56 @@ void free_list(list* lst) {
     lst = NULL;
 }
 
+/* Updates the given game moves list by deleting all nodes beyond the current pointer
+ * update current and tail field to point to newTail and adding it to end of the moves list */
+void delete_next_nodes(node* newTail, list* lst) {
+    node* currentToDel;
+    if (newTail == NULL){
+        return;
+    }
+    currentToDel = lst->current->next;
+    /* adding newTail to the end of the list */
+    newTail->next = NULL;
+    newTail->prev = lst->current;
+    lst->tail = newTail;
+    lst->current->next = newTail;
+    lst->current = newTail;
+    if (currentToDel == NULL) {
+        return; /* no nodes to delete */
+    } else {
+        while (currentToDel->next != NULL) {
+            currentToDel = currentToDel->next;
+            free_board(currentToDel->prev->board); /* deleting nodes by  memory freeing */
+            free(currentToDel->prev);
+        }
+    }
+    free_board(currentToDel->board);
+    free(currentToDel);
+}
+
+/* Updates given game moves list by generating a new node based on given board and adding it to the end of the list
+ * if undo was made and then a new move was made, redo moves are deleted. */
+void insert_at_tail(cell** board, list* lst) {
+    node* temp = lst->current;
+    node* newNode = create_node(board);
+    if (temp == lst->head) { /* first move of the game*/
+        lst->head->next = newNode;
+        newNode->prev = lst->head;
+        lst->current = newNode;
+        lst->tail = newNode;
+        return;
+    } else if (temp != lst->tail){ /* undo was made and then a new move was made */
+        delete_next_nodes(newNode, lst); /* deleting available redos moves */
+    } else { /* standard move */
+        lst->tail->next = newNode;
+        newNode->prev = lst->tail;
+        lst->tail = newNode;
+        lst->current = newNode;
+    }
+}
+
+ /* Prints messages corresponding moves made from oldBoard to create newBoard
+  * cmdType representing redo or undo command*/
 void print_board_changes(cell** oldBoard, cell** newBoard, char* cmdType) {
     int i, j, oldVal, newVal;
     int N = blockRows * blockCols;
@@ -108,8 +133,11 @@ void print_board_changes(cell** oldBoard, cell** newBoard, char* cmdType) {
     }
 }
 
-void redo(list * lst, cell **board, char mode){
-    node * newCurrent;
+/* updates given sudoku board and moves list by redoing next move available in moves list.
+ * if no move available to redo (current has no next) print appropriate messages.
+ * if a move was redo prints new board and changes made. */
+void redo(list* lst, cell** board, char mode){
+    node* newCurrent;
     if ((lst->current == lst->tail) || (lst->head->next == NULL)) {
         printf("Error: no moves to redo\n");
         return;
@@ -121,8 +149,11 @@ void redo(list * lst, cell **board, char mode){
     lst->current = newCurrent;
 }
 
-void undo(list * lst, cell **board, char mode){
-    node * newCurrent;
+/* updates given sudoku board and moves list by undoing the last move available in moves list.
+ * if no move available to undo (current has no prev) print appropriate messages.
+ * if a move was undo prints new board and changes made. */
+void undo(list* lst, cell** board, char mode){
+    node* newCurrent;
     if ((lst->current == lst->head) || (lst->head->next == NULL)){
         printf("Error: no moves to undo\n");
         return;
@@ -134,10 +165,12 @@ void undo(list * lst, cell **board, char mode){
     lst->current = newCurrent;
 }
 
-void reset(list * lst, cell **board, char mode){
+/* updates given sudoku board and moves list by resetting board to originally loaded or generated board (head of the list).
+ * all nodes of moves list except for the head are deleted.
+ * prints the original board. */
+void reset(list* lst, cell** board, char mode){
     delete_next_nodes(lst->head, lst);
     copy_board(lst->current->board, board);
     print_board(board, mode);
     printf("Board reset\n");
-
 }
--- linked_list.h
+++ linked_list.h
@@ -1,37 +1,103 @@
+/** @file linked_list.h
+ *  @brief linked list header file.
+ *
+ *  This header contains the prototypes for the linked list module.
+ *
+ *  @author Itay Keren (itaykeren)
+ *  @author Rotem Bar (rotembar)
+ *
+ */
+
+#ifndef SUDOKU_LINKED_LIST_H
+#define SUDOKU_LINKED_LIST_H
+
+
+/* -- Includes -- */
 #include "main_aux.h"
 
-#ifndef SUDOKU90_LINKED_LIST_H
-#define SUDOKU90_LINKED_LIST_H
 
+/* -- Structs -- */
+/**
+ * @brief Node structure of a linked list.
+ * each node is use to represent a game move.
+ * node contains board field to save board status after a move was made.
+ * node contains next field to point on next node (move) on linked list (moves list) a potential redo move.
+ * node contains prev field to point on prev node (move) on linked list (moves list) a potential undo move.
+ * if there are no prev or next nodes fields point to NULL.
+ * Detailed explanation.
+ */
 typedef struct node {
-    cell ** board;
+    cell** board;
     struct node *next;
     struct node *prev;
 }node;
 
+
+/**
+ * @brief List structure of a linked list.
+ * list is used to represent game moves list
+ * list contains head field to point at node representing the beginning of a list (origin board of the game).
+ * list contains current field to point on node representing the current move of a list (last move which was made including redos and undos).
+ * list contains tail field to point on node representing the last move of a list (last move which was made not including redos and undos ).
+ * if tail field != current field undos were made by the user. after new move will be made redos will be deleted.
+ * if no moves were made tail points to NULL
+ * Detailed explanation.
+ */
 typedef struct list {
-    node *head;
-    node *current;
-    node *tail;
+    node* head;
+    node* current;
+    node* tail;
 }list;
 
-node* create_node(cell **board);
 
-list* create_list(cell **board);
-
-void insert_at_tail(cell **board, list *lst);
-
-void delete_next_nodes(node* new_tail, list *lst);
-
-void free_list(list * lst);
-
-void print_board_changes(cell ** oldBoard, cell ** newBoard, char* cmdType);
-
-void redo(list * lst, cell **board, char mode);
-
-void undo(list * lst, cell **board, char mode);
-
-void reset(list * lst, cell **board, char mode);
+/** @brief Returns a new game moves list for a new sudoku game.
+ *  @param starting board of the game.
+ *  @return moves list.
+ */
+list* create_list(cell** board);
 
 
-#endif /*SUDOKU90_LINKED_LIST_H*/
+/** @brief Updates ame moves list with a new move to be added at the end of the list.
+ *  if undos were made deletes available redos.
+ *  @param board sudoku board status after a move was made.
+ *  @param lst moves list to be updated.
+ *  @return void.
+ */
+void insert_at_tail(cell** board, list* lst);
+
+
+/** @brief Frees memory of given game moves list.
+ *  @param lst moves list to be freed.
+ *  @return void.
+ */
+void free_list(list* lst);
+
+/** @brief Updates game moves list and given sudoku board by redoing last move.
+ *  @parm lst moves list which contains move to be redo to be updated.
+ *  @param board sudoku board to be updated after move was redone.
+ *  @param mode game mode.
+ *  @return void.
+ */
+void redo(list* lst, cell** board, char mode);
+
+
+/** @brief Updates game moves list and given sudoku board by undoing last move.
+ *  @parm lst moves list which contains move to be undo to be updated.
+ *  @param board sudoku board to be updated after move was undone.
+ *  @param mode game mode.
+ *  @return void.
+ */
+void undo(list* lst, cell** board, char mode);
+
+
+/** @brief Updates game moves list and given sudoku board by resetting board to origin board
+ *  deleting all moves except for the origin board.
+ *  @parm lst moves list which contains origin board at the haed of the list.
+ *  @param board sudoku board to be updated after reseted to origin.
+ *  @param mode game mode.
+ *  @return void.
+ */
+void reset(list* lst, cell** board, char mode);
+
+
+#endif /*SUDOKU_LINKED_LIST_H*/
